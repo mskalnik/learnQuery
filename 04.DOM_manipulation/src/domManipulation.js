e35e11e02756211bce565f702c82f70c4ee67c96
Marino Skalnik <marino.skalnik@gmail.com>
2020-08-30 18:16:13 +0200

    Solve the dom manipulation task

--- 04.DOM_manipulation/src/domManipulation.js
+++
@@ -1,3 +0,0 @@
-var dom = function(){
-  // code goes here
-};
---
+++ 04.DOM_manipulation/src/domManipulation.ts
@@ -0,0 +1,22 @@
+class Dom {
+  remove(element: HTMLElement) {
+    return element.remove();
+  }
+  append(targetElement: HTMLElement, element: HTMLElement) {
+    return targetElement.append(element)
+  }
+  after(targetElement: HTMLElement, element: HTMLElement) {
+    return targetElement.after(element);
+  }
+  prepend(targetElement: HTMLElement, element: HTMLElement) {
+    return targetElement.prepend(element);
+  }
+  before(targetElement: HTMLElement, element: HTMLElement) {
+    return targetElement.before(element);
+  }
+  val(targetElement: HTMLInputElement) {
+    return targetElement.value;
+  }
+}
+
+const dom = new Dom();
